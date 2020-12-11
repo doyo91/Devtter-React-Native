03418f84ca9ae49f156fc8e486e6aa0e6f4a7e71
doyo91 <samuel.gil91dev@gmail.com>
2020-12-11 12:06:14 +0100

    Bottom navigation: Tab navigator

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,25 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc"
+const tintColorDark = "#fff"
+
+export const red = "#E63946"
+export const backgroundColor = "#F1FAEE"
+export const primary = "#457B9D"
+export const primary300 = "#A8DADC"
+export const primary700 = "#1D3557"
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
-};
+}
